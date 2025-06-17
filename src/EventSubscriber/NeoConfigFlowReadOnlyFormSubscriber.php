bd183924d3aedf61d1dd892ceef79a9639cea21b
JaceRider <jacerider@gmail.com>
2025-06-17 16:17:32 -0500

    📝 (NeoConfigFlowReadOnlyFormSubscriber.php): update status message to include a period at the end for better readability and consistency

--- src/EventSubscriber/NeoConfigFlowReadOnlyFormSubscriber.php
+++ src/EventSubscriber/NeoConfigFlowReadOnlyFormSubscriber.php
@@ -31,7 +31,7 @@ class NeoConfigFlowReadOnlyFormSubscriber extends ReadOnlyFormSubscriber {
         }
         if ($form_object instanceof ConfigFormBase) {
           $editable_config = $event->getEditableConfigNames();
-          \Drupal::messenger()->addStatus(t('<strong class="text-sm">CONFIG LOCK:</strong><br> The submission of this form is prevented as changes are stored in config and need to move through the configuration workflow. You can allow submitting of this form by <a href=":url">whitelisting</a> the configs used on this page: %config/', [
+          \Drupal::messenger()->addStatus(t('<strong class="text-sm">CONFIG LOCK:</strong><br> The submission of this form is prevented as changes are stored in config and need to move through the configuration workflow. You can allow submitting of this form by <a href=":url">whitelisting</a> the configs used on this page: %config.', [
             ':url' => Url::fromRoute('config_ignore.settings')->toString(),
             '%config' => implode(', ', $editable_config),
           ]));
